533a92266c8e273915888142aa51d3f7aadcb654
Paria Razavi <paria.razavi29@gmail.com>
2025-08-06 05:51:51 +0330

    update so it tales grayscale

--- dataset_synapse.py
+++ dataset_synapse.py
@@ -4,9 +4,8 @@ import h5py
 import numpy as np
 import torch
 from scipy import ndimage
-from scipy.ndimage.interpolation import zoom
+from scipy.ndimage import zoom
 from torch.utils.data import Dataset
-
 
 def random_rot_flip(image, label):
     k = np.random.randint(0, 4)
@@ -17,13 +16,11 @@ def random_rot_flip(image, label):
     label = np.flip(label, axis=axis).copy()
     return image, label
 
-
 def random_rotate(image, label):
     angle = np.random.randint(-20, 20)
     image = ndimage.rotate(image, angle, order=0, reshape=False)
     label = ndimage.rotate(label, angle, order=0, reshape=False)
     return image, label
-
 
 class RandomGenerator(object):
     def __init__(self, output_size):
@@ -36,23 +33,20 @@ class RandomGenerator(object):
             image, label = random_rot_flip(image, label)
         elif random.random() > 0.5:
             image, label = random_rotate(image, label)
-        # 修改
-        # x, y = image.shape
+
+        if image.ndim == 2:
+            image = np.expand_dims(image, axis=-1)
+
         x, y, _ = image.shape
+
         if x != self.output_size[0] or y != self.output_size[1]:
-            # 修改
-            # image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)
             image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y, 1), order=3)
             label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
-        # 修改
-        # image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)
-        image = torch.from_numpy(image.astype(np.float32))
-        image = image.permute(2, 0, 1)
 
+        image = torch.from_numpy(image.astype(np.float32)).permute(2, 0, 1)
         label = torch.from_numpy(label.astype(np.float32))
-        sample = {'image': image, 'label': label.long()}
-        return sample
 
+        return {'image': image, 'label': label.long()}
 
 class Synapse_dataset(Dataset):
     def __init__(self, base_dir, list_dir, split, transform=None):
@@ -65,27 +59,16 @@ class Synapse_dataset(Dataset):
         return len(self.sample_list)
 
     def __getitem__(self, idx):
-        if self.split == "train":
-            slice_name = self.sample_list[idx].strip('\n')
-            data_path = os.path.join(self.data_dir, slice_name + '.npz')
-            data = np.load(data_path)
-            image, label = data['image'], data['label']
-        else:
-            slice_name = self.sample_list[idx].strip('\n')
-            data_path = os.path.join(self.data_dir, slice_name + '.npz')
-            data = np.load(data_path)
-            image, label = data['image'], data['label']
-            # 修改
-            # vol_name = self.sample_list[idx].strip('\n')
-            # filepath = self.data_dir + "/{}.npy.h5".format(vol_name)
-            # data = h5py.File(filepath)
-            # image, label = data['image'][:], data['label'][:]
-            image = torch.from_numpy(image.astype(np.float32))
-            image = image.permute(2, 0, 1)
-            label = torch.from_numpy(label.astype(np.float32))
+        slice_name = self.sample_list[idx].strip('\n')
+        data_path = os.path.join(self.data_dir, slice_name + '.npz')
+        data = np.load(data_path)
+        image, label = data['image'], data['label']
+
+        if image.ndim == 2:
+            image = np.expand_dims(image, axis=-1)
 
         sample = {'image': image, 'label': label}
         if self.transform:
             sample = self.transform(sample)
-        sample['case_name'] = self.sample_list[idx].strip('\n')
+        sample['case_name'] = slice_name
         return sample
